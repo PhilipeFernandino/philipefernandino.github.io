98356e2fa391a2547e787ec4b8477d85ed33420e
PhilipeFernandino <philipegarciaf@gmail.com>
2024-03-05 19:21:35 -0300

    feat: clash

---
+++ Connect/Clash/network.js
@@ -0,0 +1,9 @@
+//You can see a list of compatible EVM chains at https://chainlist.org/
+window.networks = [
+  {
+    id: 97,
+    label: "BNB Smart Chain Testnet BSC",
+    token: "tBNB",
+    rpcUrl: 'https://data-seed-prebsc-1-s1.bnbchain.org:8545',
+  }
+]
